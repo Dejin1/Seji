ccb4ebd3b535e1c744d86c455f3d90385cd6cff9
JinKazuma <zeath17@yahoo.com>
2019-08-26 06:14:38 -0400

    Added Scriptable Objects for Units

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,4 +6,6 @@ public static class GameManager
 {
     public static float MusicVolume = 1f;
     public static float SFXVolume = 1f;
+    public static int currency = 0;
+    public static float population = 0.0f;
 }
